f430a61466de49e746dfa3f41be56088078c5dd1
omarbougarne <omarbougarne98@gmail.com>
2025-08-04 21:56:06 +0100

    Feat a first test for routing of user module simple post and get method for initial testing purposes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ApiResponse } from 'src/common/types/api-response.type';
+import { UserDocument } from './schema/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get()
+    async findAll(): Promise<ApiResponse<UserDocument[]>> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string): Promise<ApiResponse<UserDocument>> {
+        return this.userService.findById(id);
+    }
+
+    @Post()
+    async create(@Body() createUserDto: CreateUserDto): Promise<ApiResponse<UserDocument>> {
+        return this.userService.create(createUserDto);
+    }
+}
